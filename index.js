f3f2daf5be20d699cf0fa17a454a7af3b151657b
Hriday Sarma <arroundyou.main@gmail.com>
2021-07-12 20:02:01 +0530

    Major Update

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-// $(window).resize(function () {
-//   const width = $("body").width();
-//   if (width <= 1234) {
-//     $("#media_div").html("<img src='/pics/345.jpg' class='media'/>");
-//   } else {
-//     $("#media_div").html(
-//       '<video src="./video/Pexels Videos 1654216.mp4"  mute class="media"/>'
-//     );
-//     $("#media_div img").css("max-height", "100vh");
-//   }
-// });
 
 
 var video = document.getElementById('theVideo');
@@ -25,3 +14,26 @@ $(window).resize(function() {
   video.load();
   video.play();
 });
+
+/// Form Text Field initialization ///
+
+var edtFirstName = document.getElementById('firstname');
+var edtLastName = document.getElementById('lastname');
+var edtPhone = document.getElementById('phone');
+var edtEmail = document.getElementById('email');
+var edtPassword = document.getElementById('createpassword');
+
+
+
+/// Form Text Field initialization ///
+
+var registerBtn = document.getElementById('registerBtn');
+registerBtn.onClick = function(){
+  validateInfo();
+}
+
+function validateInfo(){
+  if (edtFirstName.text == "" || edtLastName.text== "" || edtPhone == "" || edtEmail == "" || edtPassword == "") {
+
+  }
+}
